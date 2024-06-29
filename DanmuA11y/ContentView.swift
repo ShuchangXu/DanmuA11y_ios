1b7fd2628b646aa0606fc08ab41a3df2eb2d2ab2
ShuchangXu <676704276@qq.com>
2024-06-29 10:54:50 +0800

    Initial Commit

---
+++ DanmuA11y/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DanmuA11y
+//
+//  Created by 许书畅 on 2024/6/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DanmuA11y/DanmuA11yApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanmuA11yApp.swift
+//  DanmuA11y
+//
+//  Created by 许书畅 on 2024/6/29.
+//
+
+import SwiftUI
+
+@main
+struct DanmuA11yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
